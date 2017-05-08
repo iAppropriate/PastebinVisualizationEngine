f967ed7d0b36199c16965c2ad87eb4799f7cda8d
sam <shm>
2017-05-08 15:22:18 -0400

    typos

--- makeAmericaGreat.sh
+++ makeAmericaGreat.sh
@@ -1,10 +1,11 @@
-#script to grab only USA lines form input.csv
+#grab only USA lines from input.csv and store them into usa.csv
 cat input.csv |grep "United States" >.tmpusa 
 head -n 1 input.csv >usa.csv
 cat .tmpusa >>usa.csv
 rm .tmpusa
 echo "Filtering input.csv into usa.csv"
 
+#grab only selected countries from input.csv and store them into countries.csv
 cat input.csv|grep 'United States\|Russia\|China\|Brazil\|France\|United Kingdom\|India\|Australia\|Canada\|Germany\|South Africa' >.countries
 head -n 1 input.csv >countries.csv
 cat .countries >> countries.csv
